8f791fb00a952bed74c14452bd13610a566a87e0
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2018-11-02 21:01:30 +0100

    Add methods, add review done by user and check if news was reviewed by all mandatory reviewers

--- src/main/java/springfive/cms/domain/models/News.java
+++ src/main/java/springfive/cms/domain/models/News.java
@@ -1,5 +1,6 @@
 package springfive.cms.domain.models;
 
+import java.util.HashSet;
 import java.util.Set;
 import lombok.Data;
 
@@ -9,8 +10,19 @@ public class News {
     String title;
     String content;
     User author;
-    Set<User> mandatoryReviewers;
-    Set<Review> reviewers;
-    Set<Category> categories;
-    Set<Tag> tags;
+    Set<User> mandatoryReviewers = new HashSet<>();
+    Set<Review> reviewers = new HashSet<>();
+    Set<Category> categories = new HashSet<>();
+    Set<Tag> tags = new HashSet<>();
+
+    public Boolean revised() {
+        return this.mandatoryReviewers.stream().allMatch( reviewer -> this.reviewers.stream()
+                .anyMatch( review -> reviewer.id.equals(review.userId) && "approved".equals(review.status)));
+    }
+
+    public Review review(String userId, String status) {
+        final Review review = new Review(userId, status);
+        this.reviewers.add(review);
+        return review;
+    }
 }
