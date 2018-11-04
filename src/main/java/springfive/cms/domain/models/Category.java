dbf164df1a23cf13930607382404740aafbc527b
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2018-11-04 19:43:35 +0100

    Add mapping category model, and jpa repositories

--- src/main/java/springfive/cms/domain/models/Category.java
+++ src/main/java/springfive/cms/domain/models/Category.java
@@ -2,8 +2,23 @@ package springfive.cms.domain.models;
 
 import lombok.Data;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Data
+@Entity
+@Table(name = "category")
 public class Category {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
     String id;
+
     String name;
+
 }
--- src/main/java/springfive/cms/domain/repository/CategoryRepository.java
+++ src/main/java/springfive/cms/domain/repository/CategoryRepository.java
@@ -1,8 +1,14 @@
 package springfive.cms.domain.repository;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import springfive.cms.domain.models.Category;
 
-@Service
-public class CategoryRepository extends AbstractRepository<Category> {
+public interface CategoryRepository extends JpaRepository<Category, String> {
+
+    List<Category> findByName(String name);
+
+    List<Category> findByNameIgnoreCaseStartingWith(String name);
+
 }
